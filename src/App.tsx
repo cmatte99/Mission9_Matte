2e005edeb2ec58422459331929e23cf79e926c2b
Chad Sauder <chadsauder22@gmail.com>
2024-03-07 20:38:02 -0700

    I set up the default setup of React

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          NCAA Basketball
         </a>
       </header>
     </div>
