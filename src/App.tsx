dbc2b6d83e33ad425859f395b9fbdd5c6ef5ce50
Chad Sauder <chadsauder22@gmail.com>
2024-03-08 22:04:55 -0700

    with comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './App.css';
 import teamData from './CollegeBasketballTeams.json';
 
+// Define the Team interface
 interface Team {
   school: string;
   mascot: string;
   location: string;
 }
 
+// Component for displaying a welcome message
 function Welcome() {
-  return <h1>NCAA College Teams on March Madness</h1>;
+  return <h1>NCAA College Teams: March Madness</h1>;
 }
 
+// Component for displaying information about a single team
 class TeamCard extends React.Component<Team> {
   render() {
+    // Destructure the props
     const { school, mascot, location } = this.props;
     return (
       <div>
@@ -25,9 +29,11 @@ class TeamCard extends React.Component<Team> {
   }
 }
 
+// Component for displaying a list of teams
 function TeamList() {
   return (
     <div className="Team">
+      {/* Map through the teams and render TeamCard for each */}
       {teamData.teams.map((team: any, index: number) => (
         <TeamCard
           key={index}
@@ -41,11 +47,14 @@ function TeamList() {
   );
 }
 
+// Main App component
 function App() {
   return (
     <div className="App">
       <>
+        {/* Render the Welcome component */}
         <Welcome />
+        {/* Render the TeamList component */}
         <TeamList />
       </>
     </div>
